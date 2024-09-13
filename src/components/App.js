e49fc271b5735ec362af0748dd8bc120c5c39997
Hafizzargar <hafezzargar987@gmail.com>
2024-09-13 14:52:18 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
 
     const login = () => {
-        setCurrentUser('rohan');
+        setCurrentUser('Hafizzargar');
         setIsAuthenticated(true);
     };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
 
 
-const rootElement = document.getElementById('root');
+// const rootElement = document.getElementById('root');
 
-// Render the App component on the root DOM node
-ReactDOM.render(<App />, rootElement);
+// // Render the App component on the root DOM node
+// ReactDOM.render(<App />, rootElement);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
